fd1d540f585817e2ff7e015ba2ef48aaf70ea37e
Bernard Allotey <benard.allotey4@yahoo.com>
2015-04-07 00:44:30 +0000

    Made it better

--- examples/ex15.py
+++ examples/ex15.py
@@ -1,17 +1,27 @@
-from sys import argv
-
-script, filename = argv
-
+#Importing system commands so I can close the program if I need to.
+import time
+import sys 
+#Just a little art
 prompt = '---->'
-
+#Ask for file
+print "Put the filename in here with .txt at the end."
+filename = raw_input(prompt)
+#opens file that was in argument as a text file
 txt = open(filename)
-
-print "Here's your file %r:" % filename
-print txt.read()
-
-print "Type the filename again:"
-file_again = raw_input(prompt)
-
-txt_again = open(file_again)
-
-print txt_again.read()
+print "Opening"
+time.sleep(1)
+print "."
+time.sleep(1)
+print "."
+time.sleep(1)
+print "."
+time.sleep(1)
+print "Done!"
+#Prints out file
+print "File ready! %r: Type OK if you want to read it" % filename
+choice = raw_input(prompt)
+if 'OK' in choice:
+    print txt.read()
+else:
+    print "OK"
+    sys.exit()
