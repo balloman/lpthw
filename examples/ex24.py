7fda711b96db0c122c1c4c10ca39f7cdf293c124
Beranrd Allotey <balloman@koding.com>
2015-04-11 16:22:23 +0000

    Some stuff

---
+++ examples/ex24.py
@@ -0,0 +1,10 @@
+print "Lets practice everything."
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+poem = """
+\tThe Lovely World
+with logic so firmly planted
+cannot disceern \n the needs of Love
+nor comprehend passion from intuition
+and requires an explanation
+\n\twhere there is more
