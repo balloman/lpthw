4c8112959f246d7a71cfd1393ad88f51b57f1af9
Bernard Allotey <benard.allotey4@yahoo.com>
2015-06-17 20:26:18 -0500

    New Stuff for the editor.

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-﻿from time import sleep
+from time import sleep
 
 #Editor for the file
 def file_editor():
     print "Hello and welcome to the Python file creator."
     print "What is the name file that should be made."
     fname = raw_input(">>> ") + ".txt"
-    with open(fname, "w") as f:
-        print "What would you like to write in the file"
-        s = raw_input(">>> ")
-        f.write(s + "\n")
+    f = open(fname, "w")
+    print "What would you like to write in the file"
+    s = raw_input(">>> ")
+    f.write(s + "\n")
+    f.close()
     starter()
 
 def editor():
@@ -20,20 +21,29 @@ def editor():
     print "." 
     sleep (1.5)
     print "."
-    with open(fname, "r+") as f:
-        sleep(1.5)
-        print "."
-        sleep(1)
-        print "File Opened!"
-        sleep(0.5)
-        print "File reads."
-        print (f.read())
-        sleep(0.5)
-        print "What would you like to add?"
-        s = raw_input(">>> ")
-        f.seek(0,2)
-        f.write(s + "\n")
-        print "File written"
+    f = open(fname, "r+")
+    sleep(1.5)
+    print "."
+    sleep(1)
+    print "File Opened!"
+    sleep(0.5)
+    print "File reads."
+    print (f.read())
+    sleep(0.5)
+    print "What would you like to add?"
+    s = raw_input(">>> ")
+    f.seek(0,2)
+    f.write(s + "\n")
+    print "File written"
+    print "Would you like to save?"
+    answer = raw_input(">>> ")
+    if "y" in answer:
+        print "..."
+        sleep(1.25)
+        f.close()
+        print "File Saved!"
+        starter()
+    else:
         starter()
 #For the start of the program
 def start():
