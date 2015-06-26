e95d144e72ef85ca71d139eac6f3b62b611e96ca
Bernard Allotey <benard.allotey4@yahoo.com>
2015-06-25 21:39:48 -0500

    Added ex40

---
+++ examples/ex40.py
@@ -0,0 +1,20 @@
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        """Prints out the song in a for loop."""
+        for line in self.lyrics:
+            print line
+
+happy_bday = Song(["Happy birthday to you",
+                   "I don't wanna get sued",
+                   "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family",
+                        "with pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
